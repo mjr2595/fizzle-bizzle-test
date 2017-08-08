2d4681eaba24aef120fc1e5de405dcc1de54938b
Michael J Raymond <michaeljohnraymond@gmail.com>
2017-08-07 19:18:52 -0500

    initial commit

---
+++ FizzleBizzleTest/Models/FizzleBizzle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FizzleBizzleTest.Models
+{
+    public class FizzleBizzle : IFizzleBizzle
+    {
+    
+        public int Fizz     { get; set;} 
+        public int Buzz     { get; set;}
+        public int Start    { get; set;}
+        public int End      { get; set;}
+    
+        public FizzleBizzle() { }
+        
+        public string[] FizzBuzz(int start, int end)
+        {
+            return null;
+        }
+        
+        public string[] FizzBuzzBazz(int start, int end, Predicate<int> bazz)
+        {
+            return null;
+        }
+        
+    }
+}
---
+++ FizzleBizzleTest/Models/IFizzleBizzle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FizzleBizzleTest.Models
+{
+    public interface IFizzleBizzle
+    {
+        int Fizz { set; }
+        int Buzz { set; }
+            
+        string[] FizzBuzz(int start, int end);
+           
+        string[] FizzBuzzBazz(int start, int end, Predicate<int> bazz);
+    }
+}
