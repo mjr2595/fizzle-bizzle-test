ef6a0575b31ea4705973ef10927e6db86ed9584f
Michael J Raymond <michaeljohnraymond@gmail.com>
2017-08-07 22:22:07 -0500

    added bootstrap and jquery files

--- FizzleBizzleTest/Controllers/HomeController.cs
+++ FizzleBizzleTest/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
+using FizzleBizzleTest.Models;
 
 namespace FizzleBizzleTest.Controllers
 {
---
+++ FizzleBizzleTest/Scripts/app.js
@@ -0,0 +1,18 @@
+﻿var $Bazz = $("#Bazz");
+var $Predicate = $("#Predicate");
+
+function toggleBazz() {
+    $Bazz.toggle();
+    $Predicate.toggle();
+}
+
+
+// on Window Load 
+$(window).on('load', function () {
+
+    //check if Bazz field has a value 
+    if (!($Bazz.val() == 0)) {
+        toggleBazz()
+    }
+
+});
